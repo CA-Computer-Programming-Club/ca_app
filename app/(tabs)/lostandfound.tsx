bf37c97c8d6a03afa4951295342314b04d2e3763
Rehpotsirhc <86068565+Rehpotsirhc-z@users.noreply.github.com>
2025-10-08 15:17:32 -0400

    Remove duplicate styles

--- app/(tabs)/lostandfound.tsx
+++ app/(tabs)/lostandfound.tsx
@@ -3,7 +3,6 @@ import { Alert, Pressable, StyleSheet, Text } from "react-native";
 import ParallaxScrollView from "@/components/parallax-scroll-view";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
-import { styles } from ".";
 
 export default function TabTwoScreen() {
   return (
@@ -11,41 +10,41 @@ export default function TabTwoScreen() {
       headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
       showsVerticalScrollIndicator={false}
       showsHorizontalScrollIndicator={false}
-    /* headerImage={ */
-    /*   <IconSymbol */
-    /*     size={310} */
-    /*     color="#808080" */
-    /*     name="chevron.left.forwardslash.chevron.right" */
-    /*     style={styles.headerImage} */
-    /*   /> */
-    /* } */
+      /* headerImage={ */
+      /*   <IconSymbol */
+      /*     size={310} */
+      /*     color="#808080" */
+      /*     name="chevron.left.forwardslash.chevron.right" */
+      /*     style={styles.headerImage} */
+      /*   /> */
+      /* } */
     >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">
-          Lost and Found
-        </ThemedText>
+        <ThemedText type="title">Lost and Found</ThemedText>
       </ThemedView>
-      <Pressable style={{backgroundColor: "purple"}} onPress={Alert.alert(
-    'Alert Title',
-    'My Alert Msg',
-    [
-      {
-        text: 'Cancel',
-        onPress: () => Alert.alert('Cancel Pressed'),
-        style: 'cancel',
-      },
-    ],
-    {
-      cancelable: true,
-      onDismiss: () =>
-        Alert.alert(
-          'This alert was dismissed by tapping outside of the alert dialog.',
-        ),
-    },
-  )
-}>
-      <Text>+</Text>
-    </Pressable>
+      <Pressable
+        style={{ backgroundColor: "purple" }}
+        onPress={Alert.alert(
+          "Alert Title",
+          "My Alert Msg",
+          [
+            {
+              text: "Cancel",
+              onPress: () => Alert.alert("Cancel Pressed"),
+              style: "cancel",
+            },
+          ],
+          {
+            cancelable: true,
+            onDismiss: () =>
+              Alert.alert(
+                "This alert was dismissed by tapping outside of the alert dialog.",
+              ),
+          },
+        )}
+      >
+        <Text>+</Text>
+      </Pressable>
     </ParallaxScrollView>
   );
 }
@@ -60,5 +59,12 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: "row",
     gap: 8,
+  },
+  button: {
+    backgroundColor: "#14694f",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    alignItems: "center",
   },
 });

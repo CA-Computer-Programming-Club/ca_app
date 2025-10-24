532b4dfc3297255fe7c8b4be5b6871f19bfb9331
Danchaper <146667449+Danchaper@users.noreply.github.com>
2025-10-24 15:29:19 -0400

    Fix the parallax scroll view

--- components/parallax-scroll-view.tsx
+++ components/parallax-scroll-view.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren, ReactElement } from "react";
-import { StyleSheet, ScrollViewProps } from "react-native";
+import { ScrollViewProps, StyleSheet } from "react-native";
 import Animated, {
   interpolate,
   useAnimatedRef,
@@ -15,7 +15,7 @@ const HEADER_HEIGHT = 250;
 
 type Props = PropsWithChildren<
   ScrollViewProps & {
-    headerImage: ReactElement;
+    headerImage?: ReactElement;
     headerBackgroundColor: { dark: string; light: string };
   }
 >;
